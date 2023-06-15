15e8e8f8254226afa5f3e295ccaf3e7f927eb04f
Aditya Mulgundkar <adi@svoot.com>
2023-06-15 10:02:11 +0530

    Added church demos

--- src/test1.py
+++ src/test1.py
@@ -26,8 +26,8 @@ print("SSIM: {}".format(score))
 
 # threshold the difference image, followed by finding contours to
 # obtain the regions of the two input images that differ
-thresh = cv2.threshold(diff, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(diff, 100, 255,
+	cv2.ADAPTIVE_THRESH_GAUSSIAN_C | cv2.THRESH_OTSU)[1]
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
